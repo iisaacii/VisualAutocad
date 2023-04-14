61501f9bfe1ce3f108cd1953a2fe6a331302f12f
iisaacii <iisaacii@users.noreply.github.com>
2023-04-14 08:46:05 -0600

    Delete TEMAS ESPECIALES DE COMPUTACION directory

--- TEMAS ESPECIALES DE COMPUTACION/SEMINARIO DE CAD/declaraciones.vb
+++
@@ -1,5 +0,0 @@
-﻿Module declaraciones
-    Public AUTOCADAPP As AutoCAD.AcadApplication 'Este es AUTOCAD en si
-    Public DOCUMENTO As AutoCAD.AcadDocument 'Es el plano activo de AUTOCAD
-    Public UTILITY As AutoCAD.AcadUtility
-End Module
